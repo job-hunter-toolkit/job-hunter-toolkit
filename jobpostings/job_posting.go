648c0ef1cab81acbac3cc29720e1611133c896f6
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-06-09 11:36:27 -0400

    Update job_posting.go
    add tags to struct fields

--- jobpostings/job_posting.go
+++ jobpostings/job_posting.go
@@ -3,7 +3,7 @@ package jobpostings
 // JobPosting is the basic building block of
 // the functionality provided by this toolkit.
 type JobPosting struct {
-	URL      string
-	Title    string
-	Location string
+	URL      string `json:"url"`
+	Title    string `json:"title"`
+	Location string `json:"location"`
 }
