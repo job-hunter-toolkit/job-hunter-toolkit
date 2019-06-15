f98d0ee74a964203b4134d7a13a9b647dcddea71
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-06-15 13:30:23 -0400

    Create lever_helper_test.go

---
+++ jobpostings/lever_helper_test.go
@@ -0,0 +1,20 @@
+package jobpostings
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestLeverJobPostings(t *testing.T) {
+	jobPostings, err := getLeverJobsFor(context.Background(), "npm")
+	//jobPostings, err := getLeverJobsFor(context.Background(), "twitch")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for jobPosting := range jobPostings {
+		fmt.Println("title:", jobPosting.Title, "location:", jobPosting.Location, "url:", jobPosting.URL)
+	}
+}
