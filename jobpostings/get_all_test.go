3772e553114bf4a8f9a501e230307deb0b2a80e3
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-04-20 19:06:43 -0400

    Update get_all_test.go

--- jobpostings/get_all_test.go
+++ jobpostings/get_all_test.go
@@ -9,12 +9,22 @@ import (
 
 func TestGetAllJobPostings(t *testing.T) {
 	// this test is probably the only that shouldn't be running in parallel
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
 	defer cancel()
 
 	jobPostings := GetAllJobPostings(ctx)
 
 	for jobPosting := range jobPostings {
+		// spot check during testing
+		if jobPosting.URL == "" {
+			t.Fatalf("URL is empty for: %#+v", jobPosting)
+		}
+		if jobPosting.Location == "" {
+			t.Fatalf("Location is empty for: %#+v", jobPosting)
+		}
+		if jobPosting.Title == "" {
+			t.Fatalf("Title is empty for: %#+v", jobPosting)
+		}
 		fmt.Println("title:", jobPosting.Title, "location:", jobPosting.Location, "url:", jobPosting.URL)
 	}
 }
